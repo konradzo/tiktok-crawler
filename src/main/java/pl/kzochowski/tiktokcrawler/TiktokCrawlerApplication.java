02ae227fcdbe937ecccf3f02cb9fc1dfbb679118
Konrad Żochowski <zochowski.konrad@gmail.com>
2020-04-22 20:57:50 +0200

    initial commit

---
+++ src/main/java/pl/kzochowski/tiktokcrawler/TiktokCrawlerApplication.java
@@ -0,0 +1,13 @@
+package pl.kzochowski.tiktokcrawler;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TiktokCrawlerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TiktokCrawlerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pl/kzochowski/tiktokcrawler/TiktokCrawlerApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.kzochowski.tiktokcrawler;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TiktokCrawlerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
