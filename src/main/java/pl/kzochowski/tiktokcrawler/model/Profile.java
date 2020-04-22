c7b85c0296be2c7d9d3979cf6436936516beeebd
Konrad Żochowski <zochowski.konrad@gmail.com>
2020-04-22 21:19:06 +0200

    main objects

---
+++ src/main/java/pl/kzochowski/tiktokcrawler/model/Profile.java
@@ -0,0 +1,12 @@
+package pl.kzochowski.tiktokcrawler.model;
+
+public class Profile {
+
+    private Integer id;
+    private String userId;
+    private String nickname;
+    private String uniqueId;
+    private String profilePageUrl;
+    private String lastExecution;
+
+}
---
+++ src/main/java/pl/kzochowski/tiktokcrawler/model/Tag.java
@@ -0,0 +1,13 @@
+package pl.kzochowski.tiktokcrawler.model;
+
+import java.time.LocalDateTime;
+
+public class Tag {
+
+    private Integer id;
+    private String tagId;
+    private String tagName;
+    private String tagPageUrl;
+    private LocalDateTime lastExecution;
+
+}
