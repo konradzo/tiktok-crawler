6ea90907c611925c3fbc98314f75fa5461b0f931
Konrad Żochowski <zochowski.konrad@gmail.com>
2020-04-22 22:38:55 +0200

    database connection

--- src/main/java/pl/kzochowski/tiktokcrawler/model/Profile.java
+++ src/main/java/pl/kzochowski/tiktokcrawler/model/Profile.java
@@ -1,12 +1,34 @@
 package pl.kzochowski.tiktokcrawler.model;
 
+import lombok.*;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import java.time.LocalDateTime;
+
+@Builder
+@EqualsAndHashCode
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
 public class Profile {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
+    @NonNull
     private String userId;
+    @NonNull
     private String nickname;
+    @NonNull
     private String uniqueId;
+    @NonNull
     private String profilePageUrl;
-    private String lastExecution;
+    @NonNull
+    private LocalDateTime lastExecution;
 
 }
--- src/main/java/pl/kzochowski/tiktokcrawler/model/Tag.java
+++ src/main/java/pl/kzochowski/tiktokcrawler/model/Tag.java
@@ -1,13 +1,32 @@
 package pl.kzochowski.tiktokcrawler.model;
 
+import lombok.*;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 import java.time.LocalDateTime;
 
+@Builder
+@EqualsAndHashCode
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
 public class Tag {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
+    @NonNull
     private String tagId;
+    @NonNull
     private String tagName;
+    @NonNull
     private String tagPageUrl;
+    @NonNull
     private LocalDateTime lastExecution;
 
 }
