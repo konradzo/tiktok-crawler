c9653b253c5d3653a72316e65eefc5fafc21487f
Konrad Żochowski <zochowski.konrad@gmail.com>
2020-05-02 20:08:22 +0200

    refactor

---
+++ src/main/java/pl/kzochowski/tiktokcrawler/model/PageUrl.java
@@ -0,0 +1,12 @@
+package pl.kzochowski.tiktokcrawler.model;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class PageUrl {
+
+    private String pageUrl;
+
+}
--- src/main/java/pl/kzochowski/tiktokcrawler/web/ProfileController.java
+++ src/main/java/pl/kzochowski/tiktokcrawler/web/ProfileController.java
@@ -7,6 +7,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
+import pl.kzochowski.tiktokcrawler.model.PageUrl;
 import pl.kzochowski.tiktokcrawler.model.Profile;
 import pl.kzochowski.tiktokcrawler.service.ProfileService;
 
@@ -22,13 +23,7 @@ public class ProfileController {
     @PostMapping("/profile")
     @ResponseStatus(HttpStatus.CREATED)
     Profile addProfile(@RequestBody PageUrl pageUrl){
-        return profileService.addProfile(pageUrl.pageUrl);
-    }
-
-    @Setter
-    @Getter
-    static class PageUrl{
-        private String pageUrl;
+        return profileService.addProfile(pageUrl.getPageUrl());
     }
 
 }
