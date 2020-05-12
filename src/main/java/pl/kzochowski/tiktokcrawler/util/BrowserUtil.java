20b45f9a461f58b8f3143535d4f17519d0d5592f
Konrad Żochowski <konrad.zochowski@newspoint.pl>
2020-05-12 18:16:37 +0200

    creating chromeDriver instance

--- src/main/java/pl/kzochowski/tiktokcrawler/util/BrowserUtil.java
+++ src/main/java/pl/kzochowski/tiktokcrawler/util/BrowserUtil.java
@@ -1,5 +1,7 @@
 package pl.kzochowski.tiktokcrawler.util;
 
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
@@ -9,10 +11,25 @@ public class BrowserUtil {
     private final String chromeDriverPath;
     private final String userAgentToRenderPage;
 
-    public BrowserUtil(@Value("${webdriver.chrome:driver/chromedriver}") String chromeDriverPath,
-                       @Value("${userAgent.toRenderPage}")String userAgentToRenderPage) {
+    public BrowserUtil(@Value("${webdriver.chrome.driver:driver/chromedriver}") String chromeDriverPath,
+                       @Value("${userAgent.toRenderPage}") String userAgentToRenderPage) {
         this.chromeDriverPath = chromeDriverPath;
         this.userAgentToRenderPage = userAgentToRenderPage;
+    }
+
+    //todo static method?
+
+    public ChromeDriver getChromeDriverInstance() {
+        //todo add to configuration class, remove this property
+        System.setProperty("webdrviver.chrome.driver", chromeDriverPath);
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--no-sandbox");
+        options.addArguments("--no-zygote");
+        options.addArguments("--single-process");
+        options.addArguments("--keep-alive-for-test");
+        options.addArguments("--headless");
+        options.addArguments("user-agent=\"" + userAgentToRenderPage + "\"");
+        return new ChromeDriver(options);
     }
 
 }
