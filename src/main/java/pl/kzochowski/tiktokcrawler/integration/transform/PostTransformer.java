fb829ee625ca6585dcea2bb674833cb8aa059620
Konrad Żochowski <konrad.zochowski@newspoint.pl>
2020-05-18 19:25:02 +0200

    transforming posts in progress

--- src/main/java/pl/kzochowski/tiktokcrawler/integration/transform/PostTransformer.java
+++ src/main/java/pl/kzochowski/tiktokcrawler/integration/transform/PostTransformer.java
@@ -5,20 +5,27 @@ import org.springframework.integration.transformer.AbstractPayloadTransformer;
 import org.springframework.stereotype.Component;
 import pl.kzochowski.tiktokcrawler.model.Document;
 import pl.kzochowski.tiktokcrawler.model.PagePostsDto;
+import pl.kzochowski.tiktokcrawler.service.DocumentTransformer;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Slf4j
 @Component
 public class PostTransformer extends AbstractPayloadTransformer<PagePostsDto, List<Document>> {
 
+    private final DocumentTransformer documentTransformer;
 
+    public PostTransformer(DocumentTransformer documentTransformer) {
+        this.documentTransformer = documentTransformer;
+    }
 
     @Override
     protected List<Document> transformPayload(PagePostsDto pagePostsDto) {
-
-        //todo implement, use custom lib
-        return null;
+        log.info("Transforming post list...");
+        return pagePostsDto.getPostList().stream()
+                .map(documentTransformer::translateToDocument)
+                .collect(Collectors.toList());
     }
 
 }
--- src/main/java/pl/kzochowski/tiktokcrawler/service/impl/DocumentTransformerImpl.java
+++ src/main/java/pl/kzochowski/tiktokcrawler/service/impl/DocumentTransformerImpl.java
@@ -1,9 +1,13 @@
 package pl.kzochowski.tiktokcrawler.service.impl;
 
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
 import pl.kzochowski.tiktokcrawler.model.Document;
 import pl.kzochowski.tiktokcrawler.model.Post;
 import pl.kzochowski.tiktokcrawler.service.DocumentTransformer;
 
+@Slf4j
+@Service
 public class DocumentTransformerImpl implements DocumentTransformer {
 
     @Override
